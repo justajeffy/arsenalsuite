47844abe2ea72f8c634e38d736be0d0bb49962c8
Barry Robison <barry.robison@gmail.com>
2010-05-21 10:15:57 +1000

    use a datestamp for release number as svn is deprecated

--- bash/drd-release.sh
+++ bash/drd-release.sh
@@ -1,5 +1,5 @@
 if [ "v$V" == "v" ]; then
-V=`svn info | grep Revision | cut -f2 -d' '`
+V=`date +%Y%m%d%H%M`
 fi
 
 if [ `uname -s` == "Linux" ]; then
@@ -15,11 +15,11 @@ fi
 echo '***' release stone libs
 STONEDIR=/drd/software/ext/stone/$ARCH/$V
 mkdir $STONEDIR
-rsync -ac $DESTDIR/usr/local/lib/libass* $STONEDIR/
-rsync -ac $DESTDIR/usr/local/lib/libabsubmit* $STONEDIR/
-rsync -ac $DESTDIR/usr/local/lib/libstone* $STONEDIR/
-rsync -ac $DESTDIR/usr/local/lib/libclasses* $STONEDIR/
-rsync -ac $DESTDIR/usr/local/lib/libbrainiac* $STONEDIR/
+rsync -avc $DESTDIR/usr/local/lib/libass* $STONEDIR/
+rsync -avc $DESTDIR/usr/local/lib/libabsubmit* $STONEDIR/
+rsync -avc $DESTDIR/usr/local/lib/libstone* $STONEDIR/
+rsync -avc $DESTDIR/usr/local/lib/libclasses* $STONEDIR/
+rsync -avc $DESTDIR/usr/local/lib/libbrainiac* $STONEDIR/
 rsync -rtvc --exclude=.svn cpp/lib/stone/include $STONEDIR/
 rsync -rtvc --exclude=.svn cpp/lib/stone/.out/*.h $STONEDIR/include/
 rsync -rtvc $DESTDIR/usr/bin/sip $STONEDIR/sip
@@ -27,10 +27,10 @@ rsync -rtvc --exclude=.svn $SIPPATH/ $STONEDIR/sip.include/
 rsync -rtvc --exclude=.svn cpp/lib/stonegui/include $STONEDIR/
 rsync -rtvc --exclude=.svn cpp/lib/stonegui/.out/*.h $STONEDIR/include/
 
-rsync -ac cpp/apps/classmaker/classmaker $STONEDIR/
-rsync -ac cpp/apps/classmaker/classmaker.ini $STONEDIR/
+rsync -avc cpp/apps/classmaker/classmaker $STONEDIR/
+rsync -avc cpp/apps/classmaker/classmaker.ini $STONEDIR/
 mkdir $STONEDIR/templates
-rsync -ac cpp/apps/classmaker/templates/* $STONEDIR/templates/
+rsync -avc cpp/apps/classmaker/templates/* $STONEDIR/templates/
 
 echo '***' release python libs
 rsync -acv --exclude=wx* $DESTDIR/$PYPATH/blur /drd/software/ext/python/$ARCH/2.5/stone/$V/
@@ -40,25 +40,25 @@ rsync -acv $DESTDIR/$PYPATH/stoneconfig.py /drd/software/ext/python/$ARCH/2.5/st
 echo '***' release applications
 DIR=/drd/software/ext/ab/$ARCH/$V
 mkdir $DIR
-rsync -c cpp/apps/assburner_1_3/assburner $DIR/ab
-rsync -c cpp/apps/assburner_1_3/assburner.ini $DIR/ab.ini
-rsync -c cpp/apps/assburner_1_3/ab-offline.py $DIR/
-rsync -ac --exclude=.svn cpp/apps/assburner_1_3/plugins/ $DIR/plugins/
-rsync -c cpp/apps/assfreezer/assfreezer $DIR/af
-rsync -c cpp/apps/assfreezer/assfreezer.ini $DIR/assfreezer.ini
-rsync -ac --exclude=.svn cpp/apps/assfreezer/afplugins/ $DIR/afplugins/
+rsync -cv cpp/apps/assburner_1_3/assburner $DIR/ab
+rsync -cv cpp/apps/assburner_1_3/assburner.ini $DIR/ab.ini
+rsync -cv cpp/apps/assburner_1_3/ab-offline.py $DIR/
+rsync -avc --exclude=.svn cpp/apps/assburner_1_3/plugins/ $DIR/plugins/
+rsync -cv cpp/apps/assfreezer/assfreezer $DIR/af
+rsync -cv cpp/apps/assfreezer/assfreezer.ini $DIR/assfreezer.ini
+rsync -avc --exclude=.svn cpp/apps/assfreezer/afplugins/ $DIR/afplugins/
 
-rsync -c cpp/apps/absubmit/absubmit $DIR/
-rsync -c cpp/apps/absubmit/py2ab.py $DIR/
-rsync -c cpp/apps/absubmit/maya/*py $DIR/
-rsync -c cpp/apps/absubmit/maya/*ui $DIR/
-rsync -c cpp/apps/absubmit/nukesubmit/nuke2AB.py $DIR/nukesubmit/
-rsync -c cpp/apps/absubmit/nukesubmit/*ui $DIR/nukesubmit/
+rsync -cv cpp/apps/absubmit/absubmit $DIR/
+rsync -cv cpp/apps/absubmit/py2ab.py $DIR/
+rsync -cv cpp/apps/absubmit/maya/*py $DIR/
+rsync -cv cpp/apps/absubmit/maya/*ui $DIR/
+rsync -cv cpp/apps/absubmit/nukesubmit/nuke2AB.py $DIR/nukesubmit/
+rsync -cv cpp/apps/absubmit/nukesubmit/*ui $DIR/nukesubmit/
 
-rsync -c python/scripts/manager.py $DIR/
-rsync -c python/scripts/reaper.py $DIR/
-rsync -c python/scripts/reclaim_tasks.py $DIR/
-rsync -c python/scripts/initab.py $DIR/
+rsync -cv python/scripts/manager.py $DIR/
+rsync -cv python/scripts/reaper.py $DIR/
+rsync -cv python/scripts/reclaim_tasks.py $DIR/
+rsync -cv python/scripts/initab.py $DIR/
 
 ln -s /drd/software/ext/ab/images $DIR/images
 
