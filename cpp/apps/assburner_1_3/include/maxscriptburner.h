3ba36b2345840efd1481f4c97df6783029b95e30
Barry Robison <barry.robison@gmail.com>
2010-09-24 14:17:49 +1000

    updates to copyright info for docs

--- cpp/apps/assburner_1_3/include/maxscriptburner.h
+++ cpp/apps/assburner_1_3/include/maxscriptburner.h
@@ -3,26 +3,26 @@
  *
  * Copyright 2003 Blur Studio Inc.
  *
- * This file is part of RenderLine.
+ * This file is part of Arsenal.
  *
- * RenderLine is free software; you can redistribute it and/or modify
+ * Arsenal is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * RenderLine is distributed in the hope that it will be useful,
+ * Arsenal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with RenderLine; if not, write to the Free Software
+ * along with Arsenal; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
 
 /*
- * $Id: maxscriptburner.h 9229 2010-01-15 00:29:14Z brobison $
+ * $Id$
  */
 
 #ifdef COMPILE_MAXSCRIPT_BURNER
--- cpp/apps/assburner_1_3/src/syncburner.cpp
+++ cpp/apps/assburner_1_3/src/syncburner.cpp
@@ -3,26 +3,26 @@
  *
  * Copyright 2003 Blur Studio Inc.
  *
- * This file is part of RenderLine.
+ * This file is part of Arsenal.
  *
- * RenderLine is free software; you can redistribute it and/or modify
+ * Arsenal is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * RenderLine is distributed in the hope that it will be useful,
+ * Arsenal is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with RenderLine; if not, write to the Free Software
+ * along with Arsenal; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
 
 /*
- * $Id: syncburner.cpp 8295 2009-05-19 20:00:06Z newellm $
+ * $Id$
  */
 
 #ifdef COMPILE_SYNC_BURNER
