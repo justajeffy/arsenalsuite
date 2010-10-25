f43ca7cdbfe6e4e78734e4d1a5ebf9fda3510513
Barry Robison <barry.robison@gmail.com>
2010-10-25 13:41:56 +1100

    visual layout for filtering per column - still needs hooked to the model

--- cpp/lib/stonegui/src/recordtreeview.cpp
+++ cpp/lib/stonegui/src/recordtreeview.cpp
@@ -22,7 +22,7 @@
  */
 
 /*
- * $Id: recordtreeview.cpp 9071 2009-11-24 00:57:41Z brobison $
+ * $Id$
  */
 
 #include <qapplication.h>
@@ -32,12 +32,87 @@
 #include <QPlastiqueStyle>
 #include <qpainter.h>
 #include <qtimer.h>
+#include <qlabel.h>
+#include <qlayout.h>
+#include <qwidget.h>
+#include <qgridlayout.h>
+#include <qboxlayout.h>
+#include <qscrollarea.h>
+#include <qscrollbar.h>
+#include <qlineedit.h>
 
 #include "iniconfig.h"
 
 #include "recordtreeview.h"
 #include "modeliter.h"
 #include "supermodel.h"
+
+RecordFilterWidget::RecordFilterWidget(QWidget * parent)
+: QScrollArea(parent)
+{
+    setFixedHeight(20);
+
+    widget = new QWidget(this);
+    layout = new QGridLayout(widget);
+    layout->setSpacing(0);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    setWidget(widget);
+    setWidgetResizable(true);
+    setFrameShape(QFrame::NoFrame);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+}
+
+void RecordFilterWidget::setupFilters(QTreeView * tree, const ColumnStruct columns [])
+{
+    mTree = tree;
+    int cnt = 0;
+    for( cnt=0; columns[cnt].name; ++cnt );
+    for( int i=0; i<cnt; i++ ) {
+        if( columns[i].filterEnabled ) {
+            mFilterMap[i] = new QLineEdit(columns[i].name);
+            //LOG_1(columns[i].name);
+        } else {
+            mFilterMap[i] = new QWidget();
+        }
+    }
+    int i = 0;
+    for (i = 0; i < mTree->header()->count(); i++) {
+        //qDebug() << "Filter: " << filters[i] << ", Pos: " << header->visualIndex(i) << endl;
+
+        if( !mTree->header()->isSectionHidden(i) ) {
+            //mFilterMap[i]->setFixedWidth( mTree->columnWidth( mTree->header()->visualIndex(i) ) );
+            mFilterMap[i]->setFixedWidth( mTree->columnWidth( i ) );
+            layout->addWidget(mFilterMap[i], 0, mTree->header()->visualIndex(i));
+        }
+    }
+
+    layout->addItem(new QSpacerItem(1, 1, QSizePolicy::Expanding, QSizePolicy::Expanding), 1, i);
+
+    QObject::connect(mTree->header(), SIGNAL(sectionResized(int, int, int)), this, SLOT(resizeColumn(int, int, int)));
+    QObject::connect(mTree->header(), SIGNAL(sectionMoved(int, int, int)), this, SLOT(moveColumn(int, int, int)));
+    QObject::connect(mTree->horizontalScrollBar(), SIGNAL(valueChanged(int)), horizontalScrollBar(), SLOT(setValue(int)));
+}
+
+void RecordFilterWidget::resizeColumn(int column, int oldValue, int newValue)
+{
+    //mFilterMap[column]->setFixedSize(newValue, mFilterMap[column]->height());
+    mFilterMap[column]->setMinimumWidth(newValue);
+    layout->update();
+}
+
+void RecordFilterWidget::moveColumn(int, int, int)
+{
+	for (int i = 0; i < mTree->header()->count(); i++) {
+		//qDebug() << "Filter: " << filters[i] << ", Pos: " << header->visualIndex(i) << endl;
+		layout->removeWidget(mFilterMap[i]);
+        if( !mTree->header()->isSectionHidden(i) )
+            layout->addWidget(mFilterMap[i], 0, mTree->header()->visualIndex(i));
+	}
+}
+
+/// -----------------
 
 int ExtTreeView::State_ShowGrid = 0x10000000;
 
@@ -48,6 +123,7 @@ ExtTreeView::ExtTreeView( QWidget * parent, ExtDelegate * delegate )
 , mShowBranches( true )
 , mShowGrid( false )
 , mAutoResizeScheduled( false )
+, mRecordFilterWidget( 0 )
 {
 	header()->setClickable( true );
 	header()->setSortIndicatorShown( true );
@@ -78,6 +154,23 @@ ExtTreeView::ExtTreeView( QWidget * parent, ExtDelegate * delegate )
 	if( !mDelegate )
 		mDelegate = new ExtDelegate(this);
 	setItemDelegate(mDelegate);
+
+    //setContentsMargins(0, 40, 0, 0);
+}
+
+void ExtTreeView::addFilterLayout()
+{
+    QWidget * parentWidget = qobject_cast<QWidget*>(parent());
+
+    QLayout * parentLayout = parentWidget->layout();
+    delete parentLayout;
+
+    QVBoxLayout * fooLay = new QVBoxLayout(parentWidget);
+    fooLay->setContentsMargins(0,0,0,0);
+    fooLay->setSpacing(0);
+
+    fooLay->addWidget(mRecordFilterWidget);
+    fooLay->addWidget(this);
 }
 
 bool ExtTreeView::eventFilter( QObject * object, QEvent * event )
@@ -517,6 +610,13 @@ void ExtTreeView::setupTreeView( IniConfig & ini, const ColumnStruct columns [] 
 	Qt::SortOrder order(Qt::SortOrder(ini.readInt("SortOrder",Qt::AscendingOrder)));
 	header()->setSortIndicator(sc,order);
 	model()->sort(sc,order);
+
+    if( !mRecordFilterWidget ) {
+        mRecordFilterWidget = new RecordFilterWidget();
+        addFilterLayout();
+    }
+    mRecordFilterWidget->setupFilters( this, columns );
+    //QTimer::singleShot( 0, this, SLOT( addFilterLayout() ) );
 }
 
 void ExtTreeView::setupTreeView( const QString & group, const QString & key, const ColumnStruct columns [] )
@@ -547,6 +647,19 @@ void ExtTreeView::saveTreeView( const QString & group, const QString & key, cons
 	IniConfig & cfg = userConfig();
 	cfg.pushSection( group + ":" + key );
 	cfg.popSection();
+}
+
+void ExtTreeView::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    if( mRecordFilterWidget )
+        mRecordFilterWidget->resize( width(), 20 );
+}
+
+void ExtTreeView::enableFilterWidget(bool enable)
+{
+    if(mRecordFilterWidget)
+        mRecordFilterWidget->setVisible(enable);
 }
 
 RecordTreeView::RecordTreeView( QWidget * parent )
