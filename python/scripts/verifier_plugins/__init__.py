8a3e8fc0f5f4d781bfe9265ad63b9039f2e6dc94
Barry Robison <barry.robison@gmail.com>
2011-03-18 09:56:19 +1100

    remove unused plugins, add maxTaskTime - sets several 3Delight/Mantra defaults

--- python/scripts/verifier_plugins/__init__.py
+++ python/scripts/verifier_plugins/__init__.py
@@ -1,21 +1,26 @@
 import traceback
 # copy all 4 lines for each plugin
-try:
-    import denyjob
-    reload(denyjob)
-except: traceback.print_exc()
+#try:
+#    import denyjob
+#    reload(denyjob)
+#except: traceback.print_exc()
 
 try:
     import adjustPriority
     reload(adjustPriority)
 except: traceback.print_exc()
 
-try:
-    import addServices
-    reload(addServices)
-except: traceback.print_exc()
+#try:
+#    import addServices
+#    reload(addServices)
+#except: traceback.print_exc()
 
 try:
     import addFastLane
     reload(addFastLane)
+except: traceback.print_exc()
+
+try:
+    import maxTaskTime
+    reload(maxTaskTime)
 except: traceback.print_exc()
