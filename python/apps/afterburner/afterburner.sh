2fc66f383acaeb5eee00badf1179a86a952d6838
Jay Munzner <jmuenzner@gmail.com>
2011-06-21 11:00:15 +1000

    Rename afterburner for consistency.

---
+++ python/apps/afterburner/afterburner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOST=$1
+
+while read e; do export "$e"; done < <(/drd/software/int/bin/launcher.sh --project hf2 --dept rnd --printEnv farm)
+
+cd /drd/software/int/sys/afterburner/source/
+/usr/bin/python2.5 afterburner.py $HOST
