b9a5ac642bfcb4af802e0bd76635f4e6cac7c013
Matt Newell <newellshk@gmail.com>
2011-09-28 10:27:13 -0700

    Roll back unintended change to blur_build.py.

--- cpp/lib/sip/blur_build.py
+++ cpp/lib/sip/blur_build.py
@@ -13,10 +13,21 @@ elif sys.platform != 'win32':
 	else:
 		plat = 'linux-g++'
 
+class SipSipTarget(SipTarget):
+    def configure_command(self):
+        SipTarget.configure_command(self)
+        if "DESTDIR" in os.environ:
+            python_version = "python" + sys.version[:3]
+            self.config += " -b %s/usr/bin" % os.environ['DESTDIR']
+            self.config += " -d %s/usr/lib/%s/site-packages" % (os.environ['DESTDIR'], python_version)
+            self.config += " -e %s/usr/include/%s" % (os.environ['DESTDIR'], python_version)
+            self.config += " -v %s/usr/share/sip" % os.environ['DESTDIR']
+
 prep = StaticTarget("sip_prepare",path,"python build.py prepare",shell=True)
-SipTarget("sip",path,False,plat)
-SipTarget("sipstatic",path,True,plat)
-RPMTarget('siprpm','sip',path,'../../../rpm/spec/sip.spec.template','1.0')
+SipSipTarget("sip",path,False,plat)
+SipSipTarget("sipstatic",path,True,plat)
+if sys.platform=="linux2":
+    rpm = RPMTarget('siprpm','sip',path,'../../../rpm/spec/sip.spec.template','1.0',["sip","sipstatic"])
 
 if __name__=="__main__":
 	build()
