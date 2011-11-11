b68c8171f17b10bb6017ea8bcb1bd24c58244e4a
Matt Newell <newellshk@gmail.com>
2011-11-11 09:50:42 -0800

    libfreezer needs libabsubmit, not absubmit.exe.

--- cpp/lib/freezer/build.py
+++ cpp/lib/freezer/build.py
@@ -5,13 +5,15 @@ from blur.build import *
 path = os.path.dirname(os.path.abspath(__file__))
 rev_path = os.path.join(path,'../..')
 
+
+
 # Python module target
 pc = SipTarget("pyfreezer",path)
 pc.pre_deps = ["libfreezer","pyclasses:install"]
 
 pcs = SipTarget("pyfreezerstatic",path,True)
 
-QMakeTarget("libfreezer",path,"libfreezer.pro",["classesui","absubmit"])
+QMakeTarget("libfreezer",path,"libfreezer.pro",["classesui","libabsubmit"])
 
 if sys.platform=="linux2":
 	rpm = RPMTarget('freezerrpm','freezer',path,'../../../rpm/spec/freezer.spec.template','1.0',["classesrpm","libfreezer","freezer","pyfreezer"])
